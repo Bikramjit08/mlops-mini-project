9589e15e4433794468f1bbb60cd78010bf5b9a5b
BIKU <biku.bikramjit1999@gmail.com>
2025-12-23 13:47:37 +0530

    add flask testing code 8

--- tests/test_flask_app.py
+++ tests/test_flask_app.py
@@ -1,10 +1,28 @@
 import unittest
+import os
+import mlflow
 from flask_app.app import app
 
 class FlaskAppTests(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
+        # -------------------------------
+        # MLflow + DagsHub authentication
+        # -------------------------------
+        dagshub_token = os.getenv("DAGSHUB_PAT")
+        if not dagshub_token:
+            raise EnvironmentError("DAGSHUB_PAT environment variable is not set")
+
+        os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
+        os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
+
+        dagshub_url = "https://dagshub.com"
+        repo_owner = "Bikramjit08"
+        repo_name = "mlops-mini-project"
+
+        mlflow.set_tracking_uri(f"{dagshub_url}/{repo_owner}/{repo_name}.mlflow")
+
         cls.client = app.test_client()
 
     def test_home_page(self):
